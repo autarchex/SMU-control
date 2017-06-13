d960d9e36fcd622774cb3e58a0e200334b3a2ed3
autarchex <autarchex@gmail.com>
2017-06-13 13:26:54 -0700

    ..

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from instruments import B2901A
+from os import listdir
 
 """Test script to verify that I have communication with SMU working"""
+alldev = listdir("/dev/")
 
-smu = B2901A("/dev/usbtmc1")
+smu = B2901A(devicepath)
 
 smu.reset()
